bd5ff8a7ddc8754690aaf6f5db553e1a851fb704
Gacho2021 <87105994+Gacho2021@users.noreply.github.com>
2022-05-17 20:20:51 -0700

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,1 +1,19 @@
+from item import Item
 
+class __Inventory():
+    def _init__(self):
+        self.items=[]
+
+    def addItem(self, i):
+        self.items.append(i)
+
+    def getItems(self):
+        return self.items
+
+    def getItemByNumber(self, number):
+        reqItem=None
+        for i in self.items:
+            if i.itemnum==number:
+                reqItem=i
+                break
+            return reqItem
