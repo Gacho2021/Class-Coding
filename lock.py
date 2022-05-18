5add0bbeab28b2c4f3c2a7ba14fa43a06e76e655
Gacho2021 <87105994+Gacho2021@users.noreply.github.com>
2022-05-17 20:22:46 -0700

    Update lock.py

--- lock.py
+++ lock.py
@@ -1,1 +1,25 @@
+class lock():
+    def __init__(self, locked, locationid, securitylevel):
+      self.locked=lockid
+      self.locationid=locactionid
+      self.securitylevel =securitylevel
+      self.status='locked'
 
+
+    def getLocked(self):
+        return self.lockid
+
+    def getLocationid(self):
+        return self.getLocationid
+
+    def getSecurityLevel(self):
+        return self.securitylevel
+
+    def getStatus(self):
+        return self.status
+
+    def setStatus(self, status):
+        self.status=status
+
+    def __str__(self):
+        return self.lockid + ' ' + self.status
