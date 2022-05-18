9cb2820df152cbf6f666727b750f141dcb4a799e
Gacho2021 <87105994+Gacho2021@users.noreply.github.com>
2022-05-17 20:23:38 -0700

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,1 +1,22 @@
+from lock import lock
+import datetime
 
+class Scan():
+    def __init__(self, lock, card):
+        self .lock=lock
+        self .card=card
+        self.date=datetime.date.today()
+        self.time=datetime.time.strftime()
+
+
+    def getLock(self):
+        return self.lock
+
+    def getCard(self):
+        return self.card
+
+    def getDate(self):
+        return self.date
+
+    def getTime(self):
+        return self.time
